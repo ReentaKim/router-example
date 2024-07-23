c37f073a4c75b85a1fda3f174ab0d21d05915396
Reenta <reenta.kim@gmail.com>
2024-07-23 14:23:36 +0900

    Restful Route

---
+++ src/page/ProductDetailPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetailPage = () => {
+    return (
+        <div>
+            <h1>Show Detail Product!</h1>
+        </div>
+    );
+};
+
+export default ProductDetailPage;
