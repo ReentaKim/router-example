cf502240614755ecbf351fc658789ab3549b2d78
Reenta <reenta.kim@gmail.com>
2024-07-23 14:27:11 +0900

    useParams 파라메터 가져오기

--- src/page/ProductDetailPage.js
+++ src/page/ProductDetailPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
 
 const ProductDetailPage = () => {
+    const params = useParams();
+    const {id} = useParams();
+    console.log(params);
     return (
         <div>
-            <h1>Show Detail Product!</h1>
+            <h1>Show Detail Product! No.{id}</h1>
         </div>
     );
 };
