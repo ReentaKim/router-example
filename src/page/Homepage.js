5e27a472a1eb86565461f3f8cb9068414b11c8f8
Reenta <reenta.kim@gmail.com>
2024-07-23 14:34:33 +0900

    useSearchParams : url 쿼리 읽기

--- src/page/Homepage.js
+++ src/page/Homepage.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 const Homepage = () => {
+    const navigate = useNavigate();
+    const goProductPage = () => {
+        navigate('/products?q=pants')
+    }
     return (
         <div>
             <h1>Homepage</h1>
             <Link to={'/about'}>GO TO ABOUT</Link>
+            <button onClick={goProductPage}>GO To product page</button>
         </div>
     );
 };
