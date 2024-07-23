768ade9f239b049d194557b634ba4abcb0ce3404
Reenta <reenta.kim@gmail.com>
2024-07-23 14:15:53 +0900

    Link, Navigate

--- src/page/Aboutpage.js
+++ src/page/Aboutpage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 
 const Aboutpage = () => {
+
+    const navigate = useNavigate();
+    const goToHomepage = () => {
+        navigate('/');
+    }
     return (
         <div>
             <h1>Aboutpage</h1>
+            <button onClick={goToHomepage}>GO TO HOMEPAGE</button>
         </div>
     );
 };
